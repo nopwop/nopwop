97d5520aee769ceb85363c2de6e9ed19a1d45db5
kazushi <Kazushi.Yoshida@outlook.com>
2021-06-27 11:37:31 +0900

    update posts-with-category.js to fit new layout

--- src/posts-with-category/nopwop-posts-with-category.js
+++ src/posts-with-category/nopwop-posts-with-category.js
@@ -2,6 +2,7 @@
 (function(global) {
   const scriptName = 'nopwop-posts-with-category'
   const styleSheetName = '/nopwop/src/posts-with-category/nopwop-posts-with-category.css'
+  const defaultThumbnail = '/nopwop/resources/no-image-icon.png'
   //const styleSheetName = 'data:text/css;base64,LyogY3NzICovCgoKLm5wd3AtcG9zdC1jb250YWluZXIgLm5wd3AtZXJyLW1zZyB7CiAgY29sb3I6IHJlZDsKfQoKLm5wd3AtcG9zdC1jb250YWluZXIgLm5wd3AtcG9zdC1jYXJkIHsKICBkaXNwbGF5OiBmbGV4Owp9CgoubnB3cC1wb3N0LWNvbnRhaW5lciAubnB3cC1jb2wtMiB7CiAgbWFyZ2luOiA4cHggOHB4Owp9CgoubnB3cC1wb3N0LWNvbnRhaW5lciAubnB3cC1leGNlcnB0IHsKICBmb250LXNpemU6IDcwJTsKfQo='
 
   if ('nopwopPostsWithCat' in global) {
@@ -22,12 +23,12 @@
 
   function postsByCategoryId(cat_id, per_page) {
     let pp = per_page || 50
-    let url = `/wp-json/wp/v2/posts?categories=${cat_id}&per_page=${pp}&_fields=id,featured_media,title,link,excerpt`
+    let url = `/wp-json/wp/v2/posts?categories=${cat_id}&per_page=${pp}&_fields=id,featured_media,title,link,excerpt,date`
     return fetch(url).then(r => r.json())
   }
 
   function postById(post_id) {
-    let url = `/wp-json/wp/v2/posts/${post_id}?_fields=id,featured_media,title,link,excerpt`
+    let url = `/wp-json/wp/v2/posts/${post_id}?_fields=id,featured_media,title,link,excerpt,date`
     return fetch(url).then(r => r.json())
   }
 
@@ -63,37 +64,52 @@
     }
 
     promise.then(function(media) {
-      let m = d.createElement('div')
-      m.classList.add('npwp-post-card')
+      let m = d.createElement('li')
+      m.classList.add('post-list-item')
+      let c = d.createElement('div')
+      c.classList.add('post-list-container')
+      m.appendChild(c)
 
-      if (media || defaultImg) { // left side
+      { // left side 
         let left = d.createElement('div')
-        left.classList.add('npwp-col-1')
+        left.classList.add('post-list-eyecatch')
+
+        let a = d.createElement('a')
+        a.classList.add('post-list-eyecatch-link')
+        a.href = info.link
 
         let i = d.createElement('img')
         i.classList.add('npwp-thumbnail')
-        i.src = (media) ? media.media_details.sizes.thumbnail.source_url : defaultImg
-        i.setAttribute('width', '64px')
-        left.appendChild(i)
-        m.appendChild(left)
-      } 
+        i.src = (media) ? media.media_details.sizes.thumbnail.source_url : (defaultImg ? defaultImg : defaultThumbnail)
+
+        left.appendChild(a).appendChild(i)
+        c.appendChild(left)
+      }
 
       { // right side
         let right = d.createElement('div')
-        right.classList.add('npwp-col-2')
+        right.classList.add('post-list-contents')
+
+        let p = d.createElement('div')
+        p.classList.add('post-list-date')
+        p.textContent = info.date.match(new RegExp('[0-9]{4}-[0-9]{2}-[0-9]{2}'))
 
         let a = d.createElement('a')
-        a.classList.add('npwp-title-anchor')
+        a.classList.add('post-list-contents-link')
         a.href = info.link
-        a.textContent = info.title.rendered
+        
+        let h = d.createElement('h3')
+        h.classList.add('post-list-header')
+        h.textContent = info.title.rendered
     
         let e = d.createElement('div')
-        e.classList.add('npwp-excerpt')
+        e.classList.add('post-list-desc')
         e.innerHTML = info.excerpt.rendered
 
-        right.appendChild(a)
+        right.appendChild(p)
+        right.appendChild(a).appendChild(h)
         right.appendChild(e)
-        m.appendChild(right)
+        c.appendChild(right)
       }
   
       parent.appendChild(m)
@@ -101,23 +117,24 @@
   }
 
   function createContainerElement(scr) {
-    let c = d.createElement('p')
-    c.classList.add('npwp-post-container')
+    let c = d.createElement('ul')
+    c.classList.add('nopwop-post-list')
     scr.parentElement.insertBefore(c, scr)
 
     let n = scr.getAttribute('name')
     let slg = scr.getAttribute('slug')
     let id = scr.getAttribute('categoryId')
     let defaultImg = scr.getAttribute('defaultImg')
+    let per_page = scr.getAttribute('count')
     if (id) {
-      postsByCategoryId(id).then(function(list) {
+      postsByCategoryId(id, per_page).then(function(list) {
         list && list.forEach(function(i) {
           appendPostInfo(c, i, defaultImg)
         })
       })
     } else if (slg) {
       categoryBySlug(slg).then(function(d) {
-        return (d && 0 < d.length) ? postsByCategoryId(d[0].id) : Promise.resolve()
+        return (d && 0 < d.length) ? postsByCategoryId(d[0].id, per_page) : Promise.resolve()
       }).then(function(list){
         list && list.forEach(function(i) {
           appendPostInfo(c, i, defaultImg)
@@ -125,7 +142,7 @@
       })
     } else if (n) {
       categoryBySearch(n).then(function(d) {
-        return (d && 0 < d.length) ? postsByCategoryId(d[0].id) : Promise.resolve()
+        return (d && 0 < d.length) ? postsByCategoryId(d[0].id, per_page) : Promise.resolve()
       }).then(function(list){
         list && list.forEach(function(i) {
             appendPostInfo(c, i, defaultImg)
